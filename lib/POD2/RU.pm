068fb6c259a29e57df7a5746d6a07e47d13c2509
Nikolay Mishin <mi@ya.ru>
2015-03-30 04:04:53 +0300

    5.18.0.1.49

--- lib/POD2/RU.pm
+++ lib/POD2/RU.pm
@@ -10,7 +10,7 @@ use IO::Interactive qw(is_interactive);
 use Encode::Locale qw(decode_argv);
 
 use 5.008_005;
-our $VERSION = '5.18.0.1.48';
+our $VERSION = '5.18.0.1.49';
 
 our @EXPORT_OK = qw(print_pod print_pods);
 
