3f7c9ac4e0e463f2c0e92b2b617865bf216adc32
Nikolay Mishin <mi@ya.ru>
2015-03-30 19:37:37 +0300

    test

--- lib/POD2/RU.pm
+++ lib/POD2/RU.pm
@@ -8,6 +8,7 @@ use base 'Exporter';
 use base 'POD2::Base';
 use IO::Interactive qw(is_interactive);
 use Encode::Locale qw(decode_argv);
+	
 
 use 5.008_005;
 our $VERSION = '5.18.0.1.49';
@@ -113,8 +114,6 @@ POD2::RU - Perl Документация по-русски
 <a href="https://travis-ci.org/mishin/POD-to-RU"><img src="https://travis-ci.org/mishin/POD-to-RU.svg?branch=master"></a>
 <a href='https://coveralls.io/r/mishin/POD-to-RU?branch=master'><img src='https://coveralls.io/repos/mishin/POD-to-RU/badge.svg?branch=master' alt='Coverage Status' /></a>
 <a href="https://camo.githubusercontent.com/fbfe24c64a9d7662a1b9fb774ead7f7fe34fec71/687474703a2f2f6370616e74732e6370616e617574686f72732e6f72672f646973742f504f44322d52552e706e67" target="_blank"><img src="https://camo.githubusercontent.com/fbfe24c64a9d7662a1b9fb774ead7f7fe34fec71/687474703a2f2f6370616e74732e6370616e617574686f72732e6f72672f646973742f504f44322d52552e706e67" alt="" data-canonical-src="http://cpants.cpanauthors.org/dist/POD2-RU.png" style="max-width:100%;"></a>
-
-
 
 =head1 SYNOPSIS
 
